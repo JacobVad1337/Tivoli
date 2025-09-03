9a56975881d8e5fd2de8f29df59aa37d8254fd78
jacobvad1337 <jacobvad@Aiwa.local>
2025-09-03 11:29:42 +0200

    test

--- src/Lesson04/Tivoli.java
+++ src/Lesson04/Tivoli.java
@@ -1,4 +1,24 @@
 package Lesson04;
 
 public class Tivoli {
+    private int height;
+    private boolean canRide;
+
+    public Tivoli(int height, boolean canRide);
+    this.height = height;
+    this.canRide = canRide;
+
+
+    public String heightInput() {
+        if (height =< 170) {
+            return "Enjoy the ride";
+        } else if (height < 170)
+            return "You can not ride";
+
 }
+    }
+
+public int heightInput() {
+    return height;
+}
+
